0c29f7f0b7b1c5659efef1badfd0dd01f9f39772
SKFaria <fariafaria737@gmail.com>
2021-08-21 19:32:07 +0600

    solved

--- index.js
+++ index.js
@@ -1,44 +1,29 @@
-
 // update total
 function updateTotal() {
-    console.log(memoryPrice.innerText, storagePrice.innerText, shippingPrice.innerText);
+    //console.log(memoryPrice.innerText, storagePrice.innerText, shippingPrice.innerText);
     const shipping = Number(shippingPrice.innerText);
     const storage = Number(storagePrice.innerText);
     const memory = Number(memoryPrice.innerText);
     const grandTotal = 1299 + memory + storage + shipping;
     document.getElementById("totalPrice").innerText = grandTotal;
     document.getElementById("discountPrice").innerText = grandTotal;
+    // apply promo button
     const applyBtn = document.getElementById("apply-btn");
     applyBtn.addEventListener("click", function () {
-        const discount = document.getElementById("discount");
-        const discountKaku = discount.value;
-        //const discountPromo = Number(discount);
-        //console.log(discountPromo);
-        if (discountKaku == "stevekaku") {
-            //console.log(discountPromo);
-            discountPromo = (grandTotal * 20) / 100;
-            const discountPrice = grandTotal - discountPromo;
+        const discount = document.getElementById("discount").value;
+        const TotalPrice = document.getElementById("totalPrice").innerText;
+        const TotalPriceNumber = parseFloat(TotalPrice);
+        if (discount == "stevekaku") {
+            const discountPromo = (TotalPriceNumber * 20) / 100;
+            const discountPrice = TotalPriceNumber - discountPromo;
             document.getElementById("discountPrice").innerText = discountPrice;
-            discount.value = " ";
+            document.getElementById("discount").value = "";
+            //discount.disable = true;
         }
-        else { console.log("incorrect") }
     });
-    // const discount = (grandTotal * 20) / 100;
-    // document.getElementById("discount").innerText = discount;
-    // const discountPrice = grandTotal - discount;
 }
-// update after click promo button
-// function updatedDiscount() {
-//     document.getElementById("discountPrice").innerText = grandTotal;
-//     discountPromo = (grandTotal * 20) / 100;
-//     const discountPrice = grandTotal - discountPromo;
-//     document.getElementById("discountPrice").innerText = discountPrice;
-// }
-// click discount button
 
-
-
-// button click for memory
+// button click for memory----------------------------------
 const btn1 = document.getElementById("memory1");
 btn1.addEventListener("click", function () {
     memoryPrice.innerText = "0";
@@ -49,7 +34,7 @@ btn2.addEventListener("click", function () {
     memoryPrice.innerText = "180";
     updateTotal()
 });
-// button click for storage
+// button click for storage------------------------------------
 const btn3 = document.getElementById("storage1");
 btn3.addEventListener("click", function () {
     storagePrice.innerText = "0";
@@ -65,7 +50,7 @@ btn5.addEventListener("click", function () {
     storagePrice.innerText = "180";
     updateTotal()
 });
-// button click for shipping or delivery charge
+// button click for shipping or delivery charge-----------------------------
 const btn6 = document.getElementById("shipping1");
 btn6.addEventListener("click", function () {
     shippingPrice.innerText = "0";
@@ -76,6 +61,3 @@ btn7.addEventListener("click", function () {
     shippingPrice.innerText = "20";
     updateTotal()
 });
-//const total = document.getElementById("totalPrice");
-//const bestPrice = document.getElementById("bestPrice");
-//const best = Number(bestPrice.value);/
